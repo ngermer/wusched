42648e2a4b525a49693da12a6ca5b2b6189488e8
Nathan Germer <germer.n@gmail.com>
2013-05-15 22:00:44 -0500

    Data generator doesn't really... do... anything. Yet.

--- data_loader/WUSCHEDDataGenerator.py
+++ data_loader/WUSCHEDDataGenerator.py
@@ -1,3 +1,4 @@
+
 class WUSCHEDDataGenerator:
   def __init__(self):
     print "Data generator initialized."
--- data_loader/updatedb.py
+++ data_loader/updatedb.py
@@ -1,14 +1,24 @@
-import WUSCHEDParser
-import WUSCHEDDataGenerator
-import time
+from WUSCHEDParser import *
+from WUSCHEDDataGenerator import *
+import datetime
 
 print "WUSCHED database update running..."
-print "Started at", time.asctime()
+start = datetime.datetime.now()
+print "Started at",start.ctime()
 
+"""
+BEGIN UPDATE
+"""
 dg = WUSCHEDDataGenerator()
 parser = WUSCHEDParser(dg)
-with open("../../wu_e_list.html") as f:
+with open("../../wu_l_list.html") as f:
   for line in f:
     parser.feed(line)
+"""
+END UPDATE
+"""
 
-print "Finished update at", time.asctime()
+end = datetime.datetime.now()
+print "Finished update at",end.ctime()
+length = end-start
+print "WUSCHED database update finished in",length
